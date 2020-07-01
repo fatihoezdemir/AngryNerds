09c2d6c57817a40e80579069455ce8d14dbf6391
David Gastager <david.gastager@motius.de>
2020-07-02 01:30:35 +0200

    Added variables for parallax calculation in class and constructor

--- backgrounditem.cpp
+++ backgrounditem.cpp
@@ -2,13 +2,24 @@
 
 #include <QPainterPath>
 
-BackgroundItem::BackgroundItem(const QPixmap &pixmap, QGraphicsItem* parent)
-    : QGraphicsPixmapItem(pixmap, parent)
+BackgroundItem::BackgroundItem(const QPixmap &pixmap, QPointF pos,
+                               qreal offset, QGraphicsItem* parent)
+    : QGraphicsPixmapItem(pixmap, parent),
+    origPos(pos),
+    parOffset(offset)
 {
-
+    this->setPos(origPos);
 }
 
 
 QPainterPath BackgroundItem::shape() const {
     return QPainterPath();
+}
+
+QPointF BackgroundItem::getPos() {
+    return origPos;
+}
+
+qreal BackgroundItem::getOffset(){
+    return parOffset;
 }
--- backgrounditem.h
+++ backgrounditem.h
@@ -3,13 +3,21 @@
 
 #include <QGraphicsItem>
 #include <QPixmap>
+#include <QPoint>
 
 class BackgroundItem : public QGraphicsPixmapItem
 {
 public:
-    explicit BackgroundItem(const QPixmap &pixmap, QGraphicsItem* parent = 0);
+    explicit BackgroundItem(const QPixmap &pixmap, QPointF pos = QPointF(0.0,0.0),
+                            qreal offset = 0.0, QGraphicsItem* parent = 0);
 
     virtual QPainterPath shape() const;
+
+    QPointF getPos();
+    qreal getOffset();
+private:
+    QPointF origPos;
+    qreal parOffset;
 };
 
 #endif // BACKGROUNDITEM_H
