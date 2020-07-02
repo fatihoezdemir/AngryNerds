471a1c8b29b5dfa8af54b86d18ea27ec1d591da1
David Gastager <david.gastager@motius.de>
2020-07-02 15:19:17 +0200

    New staticObject class without physics implementation

--- staticobject.cpp
+++ staticobject.cpp
@@ -1,6 +1,22 @@
 #include "staticobject.h"
 
-staticObject::staticObject()
+staticObject::staticObject(const QPixmap &pixmap, QPointF pos,
+                           QGraphicsItem* parent)
+    : QGraphicsPixmapItem(pixmap, parent),
+      origPos(pos)
 {
-
+    this->setPos(origPos);
 }
+
+
+QPointF staticObject::getPos() {
+    return origPos;
+}
+
+void staticObject::setOrigPos(QPointF pos) {
+    origPos = pos;
+}
+
+/*QPainterPath staticObject::shape() const {
+    return QPainterPath();
+}*/
--- staticobject.h
+++ staticobject.h
@@ -1,11 +1,24 @@
 #ifndef STATICOBJECT_H
 #define STATICOBJECT_H
 
+#include <QGraphicsItem>
+#include <QPixmap>
+#include <QPoint>
 
-class staticObject
+class staticObject : public QGraphicsPixmapItem
 {
 public:
-    staticObject();
+    explicit staticObject(const QPixmap &pixmap, QPointF pos = QPointF(0.0,0.0), QGraphicsItem* parent = nullptr);
+
+    QPointF getPos();
+    qreal getOffset();
+
+    virtual QPainterPath shape() const; // will be removed
+
+    void setOrigPos(QPointF pos);
+private:
+    QPointF origPos;
+
 };
 
 #endif // STATICOBJECT_H
