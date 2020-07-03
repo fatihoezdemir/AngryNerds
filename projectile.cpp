c4b6f770a8a090370d2a25588b6e31090a48f9c5
David Gastager <david.gastager@motius.de>
2020-07-03 20:26:34 +0200

    Not working yet

--- projectile.cpp
+++ projectile.cpp
@@ -1,6 +1,27 @@
 #include "projectile.h"
-
-Projectile::Projectile()
+#include "globalvariables.h"
+//#include "box2d/box2d.h"
+/*
+Projectile::Projectile(const QPixmap &pixmap, QPointF pos, b2World* world, QGraphicsItem* parent)
+    :  QGraphicsPixmapItem(pixmap, parent),
+      origPos(pos)
 {
+    setPos(pos);
+    // Create the object also in box2D
+    objectBodyDef = new b2BodyDef();
+    objectBodyDef->type = b2_dynamicBody;
+    b2Vec2 b2Pos = conv::p2mVec(pos);
+    objectBodyDef->position.Set(conv::p2m(pos.x()), conv::p2m(pos.y(), true));
+    objectBody = world->CreateBody(objectBodyDef);
+    b2PolygonShape objectBox;
+    objectBox.SetAsBox(conv::p2m(this->boundingRect().width()/2), conv::p2m(this->boundingRect().height()/2),
+                        b2Vec2(conv::p2m(boundingRect().width())/2, conv::p2m(-boundingRect().height())/2), 0);
 
+    objectFixture.shape = &objectBox;
+    objectFixture.restitution = 0.6;
+    objectFixture.density = 1.0f;
+    objectFixture.friction = 0.3f;
+
+    objectBody->CreateFixture(&objectFixture);
 }
+*/
--- projectile.h
+++ projectile.h
@@ -1,11 +1,12 @@
 #ifndef PROJECTILE_H
 #define PROJECTILE_H
-
-
-class Projectile
+#include "dynamicobject.h"
+/*
+class Projectile : public DynamicObject, public QGraphicsItem
 {
 public:
-    Projectile();
+    explicit Projectile(const QPixmap &pixmap, QPointF pos = QPointF(0.0,0.0), b2World* world = 0, QGraphicsItem* parent = nullptr);
 };
-
+*/
 #endif // PROJECTILE_H
+
