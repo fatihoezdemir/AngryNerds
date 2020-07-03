ac7be87ef92a535bbd6818d9b58b37addf1d55ba
David Gastager <david.gastager@motius.de>
2020-07-03 03:18:17 +0200

    StaticObject class implemented

--- staticobject.cpp
+++ staticobject.cpp
@@ -1,13 +1,21 @@
 #include "staticobject.h"
-
+#include "globalvariables.h"
 staticObject::staticObject(const QPixmap &pixmap, QPointF pos,
-                           QGraphicsItem* parent)
+                           b2World* world, QGraphicsItem* parent)
     : QGraphicsPixmapItem(pixmap, parent),
       origPos(pos)
 {
-    this->setPos(origPos);
-}
+    setPos(pos);
 
+    // Create the object also in box2D
+    objectBodyDef = new b2BodyDef();
+    //b2Vec2 b2Pos = conv::p2mVec(pos);
+    objectBodyDef->position.Set(conv::p2m(pos.x()), conv::p2m(pos.y(), true));
+    objectBody = world->CreateBody(objectBodyDef);
+    b2PolygonShape objectBox;
+    objectBox.SetAsBox(conv::p2m(boundingRect().width()/2), conv::p2m(boundingRect().height()/2));
+    objectBody->CreateFixture(&objectBox, 0.0);
+}
 
 QPointF staticObject::getPos() {
     return origPos;
@@ -17,6 +25,6 @@ void staticObject::setOrigPos(QPointF pos) {
     origPos = pos;
 }
 
-/*QPainterPath staticObject::shape() const {
+QPainterPath staticObject::shape() const {
     return QPainterPath();
-}*/
+}
--- staticobject.h
+++ staticobject.h
@@ -4,11 +4,12 @@
 #include <QGraphicsItem>
 #include <QPixmap>
 #include <QPoint>
+#include "box2d/box2d.h"
 
 class staticObject : public QGraphicsPixmapItem
 {
 public:
-    explicit staticObject(const QPixmap &pixmap, QPointF pos = QPointF(0.0,0.0), QGraphicsItem* parent = nullptr);
+    explicit staticObject(const QPixmap &pixmap, QPointF pos = QPointF(0.0,0.0), b2World* world = nullptr,  QGraphicsItem* parent = nullptr);
 
     QPointF getPos();
     qreal getOffset();
@@ -18,6 +19,10 @@ public:
     void setOrigPos(QPointF pos);
 private:
     QPointF origPos;
+
+    b2BodyDef* objectBodyDef;
+    b2Body* objectBody;
+    b2PolygonShape objectBox;
 
 };
 
