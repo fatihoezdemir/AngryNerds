1e5d2fadd1c1d80ebb69412dccf19a681d5737e4
David Gastager <david.gastager@motius.de>
2020-07-03 03:19:25 +0200

    implemented first draft of physics sim. some stuff is still off...

--- dynamicobject.cpp
+++ dynamicobject.cpp
@@ -1,6 +1,37 @@
 #include "dynamicobject.h"
+#include "globalvariables.h"
 
-DynamicObject::DynamicObject()
+DynamicObject::DynamicObject(const QPixmap &pixmap, QPointF pos,
+                           b2World* world, QGraphicsItem* parent)
+    : QGraphicsPixmapItem(pixmap, parent),
+      origPos(pos)
 {
+    setPos(pos);
 
+    // Create the object also in box2D
+    objectBodyDef = new b2BodyDef();
+    objectBodyDef->type = b2_dynamicBody;
+    b2Vec2 b2Pos = conv::p2mVec(pos);
+    objectBodyDef->position.Set(conv::p2m(pos.x()), conv::p2m(pos.y(), true));
+    objectBody = world->CreateBody(objectBodyDef);
+    b2PolygonShape objectBox;
+    objectBox.SetAsBox(conv::p2m(boundingRect().width()/2), conv::p2m(boundingRect().height()/2));
+    objectFixture.shape = &objectBox;
+    objectFixture.restitution = 0.9;
+    objectFixture.density = 1.0f;
+    objectFixture.friction = 0.3f;
+
+    objectBody->CreateFixture(&objectFixture);
+}
+
+QPointF DynamicObject::getPos() {
+    return conv::m2pVec(objectBody->GetPosition());
+}
+
+void DynamicObject::updatePos(QPointF pos) {
+    this->setPos(pos.x(), pos.y()); // Make Conversion
+}
+
+QPainterPath DynamicObject::shape() const {
+    return QPainterPath();
 }
--- dynamicobject.h
+++ dynamicobject.h
@@ -1,11 +1,29 @@
 #ifndef DYNAMICOBJECT_H
 #define DYNAMICOBJECT_H
+#include <QGraphicsItem>
+#include <QPixmap>
+#include <QPoint>
+#include "box2d/box2d.h"
 
-
-class DynamicObject
+class DynamicObject : public QGraphicsPixmapItem
 {
 public:
-    DynamicObject();
+    explicit DynamicObject(const QPixmap &pixmap, QPointF pos = QPointF(0.0,0.0), b2World* world = 0, QGraphicsItem* parent = nullptr);
+
+    QPointF getPos();
+    qreal getOffset();
+
+    virtual QPainterPath shape() const; // will be removed
+
+    void updatePos(QPointF pos);
+
+private:
+    QPointF origPos;
+
+    b2BodyDef* objectBodyDef;
+    b2Body* objectBody;
+    b2PolygonShape objectBox;
+    b2FixtureDef objectFixture;
 };
 
 #endif // DYNAMICOBJECT_H
