f65c87e6bc0197fe4e92994eea04c64891e7e17d
David Gastager <david.gastager@motius.de>
2020-07-13 12:57:34 +0200

    added zValue Positional argument

--- staticobject.cpp
+++ staticobject.cpp
@@ -1,11 +1,12 @@
 #include "staticobject.h"
 #include "globalvariables.h"
-StaticObject::StaticObject(const QPixmap &pixmap, QPointF pos,
+StaticObject::StaticObject(const QPixmap &pixmap, QPointF pos, qreal zVal,
                            b2World* world, QGraphicsItem* parent)
     : QGraphicsPixmapItem(pixmap, parent),
       origPos(pos)
 {
     setPos(pos);
+    setZValue(zVal);
     createB2D(world);
 }
 
@@ -19,6 +20,23 @@ void StaticObject::createB2D(b2World* world){
                        b2Vec2(conv::p2m(boundingRect().width())/2, conv::p2m(-boundingRect().height())/2), 0);
     objectBody->CreateFixture(&objectBox, 0.0);
 }
+
+StaticObject::StaticObject(const QPixmap &pixmap, b2PolygonShape boundPoly, qreal zVal, QPointF pos,
+                           b2World* world, QGraphicsItem* parent)
+    : QGraphicsPixmapItem(pixmap, parent),
+      origPos(pos)
+{
+    setPos(pos);
+    setZValue(zVal);
+    objectBodyDef = new b2BodyDef();
+    objectBodyDef->position.Set(conv::p2m(pos.x()), conv::p2m(pos.y(), true));
+    objectBody = world->CreateBody(objectBodyDef);
+    b2PolygonShape objectBox = boundPoly;
+    /*objectBox.SetAsBox(conv::p2m(boundingRect().width())/2, conv::p2m(boundingRect().height())/2,
+                       b2Vec2(conv::p2m(boundingRect().width())/2, conv::p2m(-boundingRect().height())/2), 0);*/
+    objectBody->CreateFixture(&objectBox, 0.0);
+}
+
 
 QPointF StaticObject::getPos() {
     return origPos;
