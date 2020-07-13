5115d0ac030e3fb72d41251a9d739fbcb96d4974
David Gastager <david.gastager@motius.de>
2020-07-13 12:58:07 +0200

    Draft of mouse input clas, will prpbly be discarded, bc nicos version works better

--- userinput.cpp
+++ userinput.cpp
@@ -1,6 +1,38 @@
 #include "userinput.h"
+#include <QCursor>
+#include <QMouseEvent>
+#include <QCoreApplication>
 
-UserInput::UserInput()
+UserInput::UserInput(Projectile* proj, QWidget *parent)
 {
-
+    QCoreApplication::instance()->installEventFilter(this);
 }
+
+
+void UserInput::mousePressEvent(QMouseEvent *event) {
+    //Position is mapped to QPointF inPoint
+    startPoint = QCursor::pos();
+}
+
+void UserInput::mouseReleaseEvent(QMouseEvent *event) {
+    //Position is mapped to QPointFOutPoint
+    //forceTranslation is done and fed to m_projectile->shoot(vector);
+    endPoint = QCursor::pos();
+    m_projectile->shoot(forceTranslation());
+}
+
+b2Vec2 UserInput::forceTranslation(qreal factor) {
+    QPointF finVec = startPoint - endPoint;
+    return (b2Vec2(static_cast<float>(finVec.x()*factor), static_cast<float>(finVec.y()*factor)));
+}
+
+bool UserInput::eventFilter(QObject *obj, QEvent *event){
+    if(event->type() == QEvent::MouseButtonPress){
+        startPoint = QCursor::pos();
+    }
+    if (event->type() == QEvent::MouseButtonRelease) {
+        endPoint = QCursor::pos();
+
+    }
+}
+
