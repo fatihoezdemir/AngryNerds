2be49647172f28e2ecf8e8de8aef1bef83036346
David Gastager <david.gastager@motius.de>
2020-07-20 00:15:51 +0200

    added level logic for respawn and next level

--- logic.cpp
+++ logic.cpp
@@ -1,6 +1,28 @@
 #include "logic.h"
 
-Logic::Logic(QObject *parent) : QObject(parent)
+Logic::Logic(location loc, Level::levelNum lev, QObject *parent)
+    : QObject(parent), cur_loc(loc), cur_lev(lev)
 {
+    mensa_1 = new Mensa(cur_lev);
+    mensa_1->view->show();
+    connect(mensa_1 ,SIGNAL(playerDeath()), this,SLOT(restart()));
+    connect(mensa_1, SIGNAL(playerWin()), this, SLOT(nextLevel()));
+}
 
+void Logic::restart() {
+    mensa_1->view->hide();
+    delete mensa_1;
+    mensa_1 = new Mensa(cur_lev);
+    connect(mensa_1 ,SIGNAL(playerDeath()), this,SLOT(restart()));
+    connect(mensa_1, SIGNAL(playerWin()), this, SLOT(nextLevel()));
+    mensa_1->view->show();
+}
+
+void Logic::nextLevel(){
+    if (cur_lev == Level::ONE) {
+        cur_lev = Level::TWO;
+    } else if (cur_lev == Level::TWO) {
+        cur_lev = Level::THREE;
+    }
+    restart();
 }
--- logic.h
+++ logic.h
@@ -1,5 +1,8 @@
 #ifndef LOGIC_H
 #define LOGIC_H
+#include "level.h"
+#include "mensa.h"
+#include "cvl.h"
 
 #include <QObject>
 
@@ -7,11 +10,21 @@ class Logic : public QObject
 {
     Q_OBJECT
 public:
-    explicit Logic(QObject *parent = nullptr);
+    enum location{BIB, CVL, MENSA};
+    explicit Logic(location loc = Logic::MENSA, Level::levelNum lev = Level::ONE, QObject *parent = nullptr);
+
+
+private:
+    Mensa* mensa_1;
+    Cvl* cvl_1;
+    location cur_loc;
+    Level::levelNum cur_lev;
 
 signals:
 
 public slots:
+    void restart();
+    void nextLevel();
 };
 
 #endif // LOGIC_H
