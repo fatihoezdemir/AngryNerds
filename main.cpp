3a04cefc0748ed6b14e306b439cccc638be8517e
Fatih Onur Ozdemir <fatih.oezdemir@tum.de>
2020-08-06 23:24:53 +0200

    Working Splash Screen and much faster app start

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <QApplication>
+#include <QGraphicsView>
+#include <QDateTime>
+#include <QMediaPlayer>
+#include "level.h"
+#include "globalvariables.h"
+#include "cvl.h"
+#include "mensa.h"
+#include "mainwindow.h"
+#include <QSplashScreen>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QSplashScreen *splash=new QSplashScreen;
+     splash->setPixmap(QPixmap(":/imgs/png/angra.png"));
+     splash->show();
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
